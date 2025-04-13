14ca368620e74f4c1c6e4a532799b54ddce39838
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-04-13 08:14:01 -0400

    Reduce font size

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,6 +1,6 @@
 function InfoBox({ children }) {
     return (
-        <div className="rounded-lg bg-[#17233E] p-4 text-center font-bold text-white shadow-[0_0_8px_#424242,0_0_8px_#424242]">
+        <div className="rounded-lg bg-[#17233E] p-4 text-center text-lg font-bold text-white shadow-[0_0_8px_#424242,0_0_8px_#424242]">
             {children}
         </div>
     );
