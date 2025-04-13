474e949a049dc846fdb8693899c97124331def9a
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-04-13 06:37:16 -0400

    Create BackgroundVideo component

---
+++ src/components/BackgroundVideo.jsx
@@ -0,0 +1,13 @@
+import bgVideo from '../assets/background.mp4';
+
+function BackgroundVideo() {
+    return (
+        <div className="fixed top-0 left-0 z-0 h-full overflow-hidden after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[rgb(0,0,0,0.3)] after:content-['test']">
+            <video className="h-full w-screen object-cover" muted loop>
+                <source src={bgVideo} type="video/mp4" />
+            </video>
+        </div>
+    );
+}
+
+export default BackgroundVideo;
