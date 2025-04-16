590a841b6371afc3b1f6c1d1d90242fd53025b37
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-04-16 08:32:30 -0400

    Add array utils

---
+++ src/utils/array.js
@@ -0,0 +1,21 @@
+function shuffle(array) {
+    array = array.slice();
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex]
+        ];
+    }
+
+    return array;
+}
+
+export { shuffle };
