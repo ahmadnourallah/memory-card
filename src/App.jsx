4d4e1f23ae520c43c0fedcda16499a54046dc563
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-04-16 08:40:56 -0400

    Set everything up in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import BackgroundVideo from "./components/BackgroundVideo";
+import { useState } from 'react';
+import BackgroundVideo from './components/BackgroundVideo';
+import Footer from './components/Footer';
+import InfoBox from './components/InfoBox';
+import Switcher from './components/Switcher';
+import StartDialog from './components/StartDialog';
+import GameContainer from './components/GameContainer';
 
 function App() {
-	return <h1>Test</h1>;
-}
+    const [bestScore, setBestScore] = useState(0);
 
+    return (
+        <>
+            <div className="wrapper font-display relative z-1 mx-auto h-screen max-w-[1400px] p-8 text-3xl">
+                <main className="text-white">
+                    <Switcher>
+                        <StartDialog />
+                        <GameContainer
+                            bestScore={bestScore}
+                            setBestScore={setBestScore}
+                        />
+                    </Switcher>
+                </main>
+                <Footer />
+            </div>
+            <BackgroundVideo />
+        </>
+    );
+}
 export default App;
